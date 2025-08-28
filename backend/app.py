51acaa65a43f31d9472d0ab3f1062b70e9ab2b4d
aman <amanworkinfo@gmail.com>
2025-08-28 22:06:08 +0530

    first commit

---
+++ backend/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pickle
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from flask_cors import CORS
+app = Flask(__name__)
+
+CORS(app)
+# 🔹 Load model + tokenizer
+model = tf.keras.models.load_model("review.keras")
+with open("tokenizer.pkl", "rb") as f:
+    tokenizer = pickle.load(f)
+
+MAXLEN = 200
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+    review = data.get("review", "")
+    threshold = data.get("threshold", 0.6)
+
+    sequence = tokenizer.texts_to_sequences([review])
+    padded = pad_sequences(sequence, maxlen=MAXLEN)
+
+    score = model.predict(padded)[0][0]
+    sentiment = "Positive" if score >= threshold else "Negative"
+
+    return jsonify({
+        "review": review,
+        "score": float(score),
+        "threshold_used": threshold,
+        "sentiment": sentiment
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
