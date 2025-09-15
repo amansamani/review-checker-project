1e35664e5cdacde1d6275cbe50ae87327a518b60
aman <amanworkinfo@gmail.com>
2025-09-15 22:31:24 +0530

    engaging comments

--- backend/app.py
+++ backend/app.py
@@ -69,5 +69,3 @@ def predict():
 
 # Do NOT include app.run() on Render
 # Render will use: gunicorn app:app --bind 0.0.0.0:$PORT
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
